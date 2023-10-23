fbb55fbc65d305fd110d2b04934a2085334315cf
MateoDedi <mateod@live.com>
2023-10-23 17:11:36 +0200

    added 2nd swiper

---
+++ script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4, // Show 4 slides at a time
+    spaceBetween: 20, // Adjust the space between slides as needed
+    navigation: {
+        nextEl: ".swiper-button-next", // Next slide button
+        prevEl: ".swiper-button-prev", // Previous slide button
+    },
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 4, // Show 4 slides at a time
+    spaceBetween: 20, // Adjust the space between slides as needed
+    navigation: {
+        nextEl: ".swiper-button-next2", // Next slide button
+        prevEl: ".swiper-button-prev2", // Previous slide button
+    },
+});
