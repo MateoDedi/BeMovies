aea7fd1ca996853a20e94eab57cc90012d9b10b3
MateoDedi <mateod@live.com>
2023-10-24 10:03:49 +0200

    finished footer

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ var swiper2 = new Swiper(".mySwiper2", {
         prevEl: ".swiper-button-prev2", // Previous slide button
     },
 });
+
+var swiper3 = new Swiper(".mySwiper3", {
+    slidesPerView: 4, // Show 4 slides at a time
+    spaceBetween: 20, // Adjust the space between slides as needed
+    navigation: {
+        nextEl: ".swiper-button-next2", // Next slide button
+        prevEl: ".swiper-button-prev2", // Previous slide button
+    },
+});
